dd3c9e4ad3436e8353da455168d133a2cb05eba2
Sebastian Stock <sestock@uos.de>
2014-07-30 11:40:58 +0200

    Added test for Submatches in JunitTestCompoundNameMatchingConstraintSolver

--- src/test/java/unify/JunitTestCompoundNameMatchingConstraintSolver.java
+++ src/test/java/unify/JunitTestCompoundNameMatchingConstraintSolver.java
@@ -21,19 +21,19 @@ public class JunitTestCompoundNameMatchingConstraintSolver {
 	public void setUp() throws Exception {
 		solver = new CompoundNameMatchingConstraintSolver();
 		vars = (CompoundNameVariable[]) solver.createVariables(5);
-
-		vars[0].setName("On", "mug1", "table1"); // "On" in world state
-		vars[1].setName("On", "?mug", "?table"); // "On" as precondition
-		vars[2].setName("get_mug",  "?mug");     // "get_mug" task
-		vars[3].setName("get_mug", "?mug");      // "grap_mug" task
-		vars[4].setName("Holding", "?mug", "rightarm1"); // "holding" effect
 		
 		nameSolver = (NameMatchingConstraintSolver) solver.getConstraintSolvers()[0];
 	}
 
 
 	@Test
-	public void test() {
+	public void testMatches() {
+		vars[0].setName("On", "mug1", "table1"); // "On" in world state
+		vars[1].setName("On", "?mug", "?table"); // "On" as precondition
+		vars[2].setName("get_mug",  "?mug");     // "get_mug" task
+		vars[3].setName("get_mug", "?mug");      // "grap_mug" task
+		vars[4].setName("Holding", "?mug", "rightarm1"); // "holding" effect
+		
 		CompoundNameMatchingConstraint cc01 = new CompoundNameMatchingConstraint(Type.MATCHES);
 		cc01.setFrom(vars[0]);
 		cc01.setTo(vars[1]);
@@ -57,7 +57,38 @@ public class JunitTestCompoundNameMatchingConstraintSolver {
 		System.out.println(vars[4].getName());
 		assertTrue("'mug1' should be propageted to first parameter of 'holding'.", 
 				vars[4].getName().equals("Holding(mug1 rightarm1)"));
-		
 	}
+	
+	@Test
+	public void testSubmatches() {
+		vars[0].setName("On", "mug1", "table1"); 
+		vars[1].setName("get_mug", "?mug", "?table");
+		vars[2].setName("get_mug",  "mug2"); 
+		vars[3].setName("get_mug", "?mug");
+		
+		CompoundNameMatchingConstraint cc01 = new CompoundNameMatchingConstraint(Type.SUBMATCHES, 
+				new int[] {0, 0, 1, 1});
+		cc01.setFrom(vars[0]);
+		cc01.setTo(vars[1]);
+		assertTrue(solver.addConstraint(cc01));
+		System.out.println("Var1: " + vars[1].getName());
+		assertTrue(vars[1].getName().equals("get_mug(mug1 table1)"));
+		
+		CompoundNameMatchingConstraint cc02 = new CompoundNameMatchingConstraint(Type.SUBMATCHES, 
+				new int[] {0, 0});
+		cc02.setFrom(vars[0]);
+		cc02.setTo(vars[2]);
+		assertFalse(solver.addConstraint(cc02));
+		
+		CompoundNameMatchingConstraint cc03 = new CompoundNameMatchingConstraint(Type.SUBMATCHES, 
+				new int[] {0, 0});
+		cc03.setFrom(vars[0]);
+		cc03.setTo(vars[3]);
+		assertTrue(solver.addConstraint(cc03));
+		System.out.println("Var3: " + vars[3].getName());
+		assertTrue(vars[3].getName().equals("get_mug(mug1)"));
+
+	}
+
 
 }
