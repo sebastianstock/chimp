6e4fbcee20033323a8f5389c198bc29687827ffa
Sebastian Stock <sestock@uos.de>
2014-08-16 21:31:56 +0200

    Created package pfd0Symbolic + Copied fluents.
    Copied Fluent.java, FluentConstraint.java and FluentNetworkSolver.java
    from package pfd0.

---
+++ src/main/java/pfd0Symbolic/FluentConstraint.java
@@ -0,0 +1,114 @@
+package pfd0Symbolic;
+
+import org.metacsp.framework.Constraint;
+import org.metacsp.framework.Variable;
+import org.metacsp.framework.multi.MultiBinaryConstraint;
+
+import simpleBooleanValueCons.SimpleBooleanValueConstraint;
+import simpleBooleanValueCons.SimpleBooleanValueVariable;
+import unify.CompoundNameMatchingConstraint;
+import unify.CompoundNameVariable;
+
+public class FluentConstraint extends MultiBinaryConstraint {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 137380711080409334L;
+
+	public static enum Type {MATCHES, DC, PRE, OPENS, CLOSES, BEFORE};
+
+	private Type type;
+	private int[] connections;
+
+	public FluentConstraint(Type type) {
+		this.type = type;
+	}
+	
+	public FluentConstraint(Type type, int[] connections) {
+		this.type = type;
+		this.connections = connections;
+	}
+
+	@Override
+	protected Constraint[] createInternalConstraints(Variable f, Variable t) {
+		if (!( f instanceof Fluent) || !(t instanceof Fluent)) return null;
+
+		if (this.type.equals(Type.MATCHES)) {
+			SimpleBooleanValueConstraint scon = 
+					new SimpleBooleanValueConstraint(SimpleBooleanValueConstraint.Type.EQUALS);
+			SimpleBooleanValueVariable bf = ((Fluent) f).getSimpleBooleanValueVariable();
+			scon.setFrom(bf);
+			SimpleBooleanValueVariable bt = ((Fluent) t).getSimpleBooleanValueVariable();
+			scon.setTo(bt);
+
+			CompoundNameMatchingConstraint ncon = 
+					new CompoundNameMatchingConstraint(CompoundNameMatchingConstraint.Type.MATCHES);
+			CompoundNameVariable nf = ((Fluent) f).getCompoundNameVariable();
+			ncon.setFrom(nf);
+			CompoundNameVariable nt = ((Fluent) t).getCompoundNameVariable();
+			ncon.setTo(nt);
+			return new Constraint[]{scon, ncon};
+			
+		} else if (this.type.equals(Type.DC)) {
+			// TODO nothing to add here?
+		} else if (this.type.equals(Type.PRE)) {
+			// TODO would it be possible to directly create NameMatchingConstraints here?
+			if (connections != null && connections.length > 0) {
+				CompoundNameMatchingConstraint ncon = 
+						new CompoundNameMatchingConstraint(CompoundNameMatchingConstraint.Type.SUBMATCHES, 
+								connections);
+				CompoundNameVariable nf = ((Fluent) f).getCompoundNameVariable();
+				ncon.setFrom(nf);
+				CompoundNameVariable nt = ((Fluent) t).getCompoundNameVariable();
+				ncon.setTo(nt);
+				return new Constraint[]{ncon};
+			} else {
+				return null;
+			}
+		} else if (this.type.equals(Type.OPENS)) {
+			if(connections != null) {
+				return new Constraint[]{createSubmatches(f, t)};
+			} else {
+				return null;
+			}
+		}
+
+		return null;
+	}
+	
+	private CompoundNameMatchingConstraint createSubmatches(Variable f, Variable t) {
+		CompoundNameMatchingConstraint ncon = 
+				new CompoundNameMatchingConstraint(CompoundNameMatchingConstraint.Type.SUBMATCHES, 
+						connections);
+		CompoundNameVariable nf = ((Fluent) f).getCompoundNameVariable();
+		ncon.setFrom(nf);
+		CompoundNameVariable nt = ((Fluent) t).getCompoundNameVariable();
+		ncon.setTo(nt);
+		return ncon;
+	}
+
+	@Override
+	public Object clone() {
+		return new FluentConstraint(type, connections);
+	}
+
+	@Override
+	public String getEdgeLabel() {
+		return this.type.toString();
+	}
+
+	@Override
+	public boolean isEquivalent(Constraint c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+
+
+
+}
---
+++ src/main/java/pfd0Symbolic/FluentNetworkSolver.java
@@ -0,0 +1,65 @@
+package pfd0Symbolic;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+import org.metacsp.framework.Constraint;
+import org.metacsp.framework.ConstraintSolver;
+import org.metacsp.framework.Variable;
+import org.metacsp.framework.multi.MultiConstraintSolver;
+
+import pfd0.TaskApplicationMetaConstraint.markings;
+import simpleBooleanValueCons.SimpleBooleanValueConstraintSolver;
+import unify.CompoundNameMatchingConstraintSolver;
+
+public class FluentNetworkSolver extends MultiConstraintSolver {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5831971530237352714L;
+
+	public FluentNetworkSolver(long origin, long horizon) {
+		super(new Class[] {FluentConstraint.class}, Fluent.class, 
+				createConstraintSolvers(origin, horizon, -1), new int[] {1, 1});
+	}
+
+	@Override
+	public boolean propagate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private static ConstraintSolver[] createConstraintSolvers(long origin, long horizon, 
+			int maxFluents) {
+		ConstraintSolver[] ret = new ConstraintSolver[] { 
+				new CompoundNameMatchingConstraintSolver(), 
+				new SimpleBooleanValueConstraintSolver(origin, horizon)};
+		return ret;
+	}
+
+	/**
+	 * @return All Fluents with the marking OPEN.
+	 */
+	public Fluent[] getOpenFluents() {
+		ArrayList<Fluent> ret = new ArrayList<Fluent>();
+		for (Variable var: getVariables()) {
+			if (var.getMarking() == markings.OPEN) {
+				ret.add((Fluent) var);
+			}
+		}
+		return ret.toArray(new Fluent[ret.size()]);
+	}
+
+	public Constraint[] getConstraintsTo(Variable to) {
+		Vector<Constraint> ret = new Vector<Constraint>();
+		for (Constraint con : this.getConstraints()) {
+			if (con.getScope().length == 2) {
+				if (con.getScope()[1].equals(to)) ret.add(con);
+			}
+		}
+		return ret.toArray(new Constraint[ret.size()]);
+	}
+
+
+}
