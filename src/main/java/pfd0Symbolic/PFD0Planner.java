77ae84622c96526dd920b9df6df95177b01da8c5
Sebastian Stock <sestock@uos.de>
2014-08-18 11:57:18 +0200

    Added and updated PFD0Planner + MetaConstraints.

---
+++ src/main/java/pfd0Symbolic/PFD0Planner.java
@@ -0,0 +1,187 @@
+package pfd0Symbolic;
+
+import java.util.Vector;
+
+import org.metacsp.framework.Constraint;
+import org.metacsp.framework.ConstraintNetwork;
+import org.metacsp.framework.Variable;
+import org.metacsp.framework.VariablePrototype;
+import org.metacsp.framework.meta.MetaConstraintSolver;
+import org.metacsp.framework.meta.MetaVariable;
+
+import pfd0Symbolic.TaskApplicationMetaConstraint.markings;
+
+
+public class PFD0Planner extends MetaConstraintSolver {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8031573555691611305L;
+
+	public PFD0Planner(long origin, long horizon, long animationTime, String[][] symbols) {
+		// Currently only FluentConstraints. Other constraint should be added later.
+		super(new Class[] {FluentConstraint.class}, 
+				animationTime, 
+				new FluentNetworkSolver(origin, horizon, symbols));
+	}
+
+
+	@Override
+	public void preBacktrack() {
+		// TODO Do I need to do something here?
+
+	}
+
+	@Override
+	public void postBacktrack(MetaVariable mv) {
+		if (mv.getMetaConstraint() instanceof TaskApplicationMetaConstraint) {
+			for (Variable v : mv.getConstraintNetwork().getVariables()) {
+				v.setMarking(markings.SELECTED);
+			}
+		}
+		else if (mv.getMetaConstraint() instanceof PreconditionMetaConstraint) {
+			for (Variable v : mv.getConstraintNetwork().getVariables()) {
+				v.setMarking(markings.UNJUSTIFIED);
+			}
+		} 
+		else if (mv.getMetaConstraint() instanceof TaskSelectionMetaConstraint) {
+			for (Variable v : mv.getConstraintNetwork().getVariables()) {
+				v.setMarking(markings.UNPLANNED);
+			}
+		} 
+	}
+
+	/**
+	 * Defines extra operations that should happen
+	 * after retracting a meta-value in the meta-CSP search (e.g., when backtracking).
+	 * @param metaVariable The {@link MetaVariable} over which backtracking occurs.
+	 * @param metaValue The meta-value that has been retracted.
+	 */
+	@Override
+	protected void retractResolverSub(ConstraintNetwork metaVariable,
+			ConstraintNetwork metaValue) {
+		FluentNetworkSolver groundSolver = (FluentNetworkSolver)this.getConstraintSolvers()[0];
+		
+		// remove added fluents
+		Vector<Variable> varsToRemove = new Vector<Variable>();
+		for (Variable v : metaValue.getVariables()) {
+			if (!metaVariable.containsVariable(v)) {
+				if (v instanceof VariablePrototype) {
+					Variable vReal = metaValue.getSubstitution((VariablePrototype)v);
+					if (vReal != null) {
+							varsToRemove.add(vReal);
+					}
+				}
+			}
+		}
+		groundSolver.removeVariables(varsToRemove.toArray(new Variable[varsToRemove.size()]));
+		
+		
+		// change CLOSED fluents back to OPEN
+		for(Constraint c : metaValue.getConstraints()) {
+			if ((c instanceof FluentConstraint) && 
+					(((FluentConstraint) c).getType() == FluentConstraint.Type.CLOSES)) {
+				((FluentConstraint) c).getTo().setMarking(markings.OPEN);
+			}
+		}
+	}
+
+	/**
+	 * Defines additional operations that should happen before
+	 * adding a meta-value in the meta-CSP search (e.g., when branching).
+	 * 
+	 * Make real {@link Variable}s from {@link VariablePrototype}s.
+	 * Involve real {@link Variable}s in the Constraints.
+	 * @param metaVariable The {@link MetaVariable} over which the search is branching.
+	 * @param metaValue The meta-value that has been selected (the branch). 
+	 */
+	@Override
+	protected boolean addResolverSub(ConstraintNetwork metaVariable,
+			ConstraintNetwork metaValue) {
+		FluentNetworkSolver groundSolver = (FluentNetworkSolver)this.getConstraintSolvers()[0];
+		
+		System.out.println("addResolverSub Constraints: ");
+		for(Constraint c : metaValue.getConstraints()) {
+			System.out.println(c);
+		}
+		
+		//Make real variables from variable prototypes
+		for (Variable v :  metaValue.getVariables()) {
+			if (v instanceof VariablePrototype) {
+				// 	Parameters for real instantiation: the first is the component 
+				//  and the second is the type, the third are the arguments of the fluent
+				String component = (String)((VariablePrototype) v).getParameters()[0];
+				String symbol = (String)((VariablePrototype) v).getParameters()[1];
+				Fluent fluent = (Fluent)groundSolver.createVariable(component);
+				
+				if (((VariablePrototype) v).getParameters().length > 2) {
+					String[] arguments = (String[])((VariablePrototype) v).getParameters()[2];
+					fluent.setName(symbol, arguments);
+				} else {
+					fluent.setName(symbol);
+				}
+
+				fluent.setMarking(v.getMarking());
+				metaValue.addSubstitution((VariablePrototype)v, fluent);
+			}
+		}
+
+		//Involve real variables in the constraints
+		for (Constraint con : metaValue.getConstraints()) {
+			Constraint clonedConstraint = (Constraint)con.clone();  
+			Variable[] oldScope = con.getScope();
+			Variable[] newScope = new Variable[oldScope.length];
+			for (int i = 0; i < oldScope.length; i++) {
+				if (oldScope[i] instanceof VariablePrototype) {
+					newScope[i] = metaValue.getSubstitution((VariablePrototype)oldScope[i]);
+				}
+				else {
+					newScope[i] = oldScope[i];
+				}
+			}
+			clonedConstraint.setScope(newScope);
+			metaValue.removeConstraint(con);
+			metaValue.addConstraint(clonedConstraint);
+		}
+		
+		return true;
+	}
+
+	@Override
+	protected double getUpperBound() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void setUpperBound() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected double getLowerBound() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void setLowerBound() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected boolean hasConflictClause(ConstraintNetwork metaValue) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void resetFalseClause() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/pfd0Symbolic/PreconditionMetaConstraint.java
@@ -0,0 +1,150 @@
+package pfd0Symbolic;
+
+import java.util.Vector;
+
+import org.metacsp.framework.Constraint;
+import org.metacsp.framework.ConstraintNetwork;
+import org.metacsp.framework.ConstraintSolver;
+import org.metacsp.framework.Variable;
+import org.metacsp.framework.meta.MetaConstraint;
+import org.metacsp.framework.meta.MetaVariable;
+
+import pfd0Symbolic.TaskApplicationMetaConstraint.markings;
+
+public class PreconditionMetaConstraint extends MetaConstraint {
+	
+	
+
+	public PreconditionMetaConstraint() {
+		super(null, null);
+	}
+
+
+	
+	/** 
+	 * @return All {@link MetaVariable}s with the marking UNPLANNED and which have no unplanned 
+	 * predecessors.
+	 */
+	@Override
+	public ConstraintNetwork[] getMetaVariables() {
+		FluentNetworkSolver groundSolver = (FluentNetworkSolver)this.getGroundSolver();
+		Vector<ConstraintNetwork> ret = new Vector<ConstraintNetwork>();
+		// for every variable that has the marking UNJUSTIFIEDD and that has no unplanned predecessors 
+		// a ConstraintNetwork is built.
+		// this becomes a task.
+		for (Variable var : groundSolver.getVariables()) {
+			if (var.getMarking() != null && var.getMarking().equals(markings.UNJUSTIFIED)) {
+				ConstraintNetwork nw = new ConstraintNetwork(null);
+				nw.addVariable(var);
+				ret.add(nw);
+			}
+		}
+		System.out.println("MetaVariables: " + ret);
+		return ret.toArray(new ConstraintNetwork[ret.size()]);
+	
+	}
+	
+
+	
+	/**
+	 * Get all values for a given {@link MetaVariable}.
+	 * 
+	 * Search for open Variables that match the dummy precondition.
+	 * Put a matches constraint between them and set the marking to SATISFIED.
+	 * 
+	 * @param metaVariable The {@link MetaVariable} for which we seek meta values.
+	 * @return All meta values for the given{@link MetaVariables}.
+	 */
+	@Override
+	public ConstraintNetwork[] getMetaValues(MetaVariable metaVariable) {
+		// possible constraint networks
+		Vector<ConstraintNetwork> ret = new Vector<ConstraintNetwork>();
+		ConstraintNetwork problematicNetwork = metaVariable.getConstraintNetwork();
+		Fluent fl = (Fluent)problematicNetwork.getVariables()[0];
+		logger.info("getMetaValues for: " + fl);
+		
+		Fluent[] openFluents = ((FluentNetworkSolver)this.getGroundSolver()).getOpenFluents();
+		String[] headNames = fl.getCompoundSymbolicVariable().getPossiblePredicateNames();
+		for (String headName : headNames) {
+			for (Fluent openFluent : openFluents)  {
+				String[] onames = openFluent.getCompoundSymbolicVariable().getPossiblePredicateNames();
+				for (String name : onames) {
+					if(headName.equals(name)) {
+						// potential match. Add matches constraint.
+						// TODO could be optimized by testing the full name.
+						ConstraintNetwork newResolver = addMatches(fl, openFluent);
+						if (newResolver != null) {
+							ret.add(newResolver);
+						}
+						continue;
+					}
+				}
+			}
+		}
+		
+		if (!ret.isEmpty()) 
+			return ret.toArray(new ConstraintNetwork[ret.size()]);
+		return null;
+	}
+	
+	/** Adds the MATCHES constraint between the two fluents.
+	 * 
+	 * @param preFluent
+	 * @param matchingFluent
+	 * @return
+	 */
+	private ConstraintNetwork addMatches(Fluent preFluent, Fluent matchingFluent) {
+		ConstraintNetwork ret = new ConstraintNetwork(null);
+		FluentConstraint con = new FluentConstraint(FluentConstraint.Type.MATCHES);
+		con.setFrom(preFluent);
+		con.setTo(matchingFluent);
+		ret.addConstraint(con);
+		return ret;
+	}
+	
+	@Override
+	public ConstraintNetwork[] getMetaValues(MetaVariable metaVariable,
+			int initial_time) {
+		return getMetaValues(metaVariable);
+	}
+
+	/**
+	 * Sets the marking of the task to DECOMPOSED (compound) or PLANNED (primitive)
+	 */
+	@Override
+	public void markResolvedSub(MetaVariable metaVariable,
+			ConstraintNetwork metaValue) {
+		// TODO if it is a primitive task, set the marking to PLANNED
+		metaVariable.getConstraintNetwork().getVariables()[0].setMarking(markings.JUSTIFIED);
+	}
+
+	@Override
+	public void draw(ConstraintNetwork network) {
+	}
+
+	@Override
+	public ConstraintSolver getGroundSolver() {
+		return this.metaCS.getConstraintSolvers()[0];
+	}
+
+	@Override
+	public String toString() {
+		return "PFD0MetaConstraint";
+	}
+
+	@Override
+	public String getEdgeLabel() {
+		return null;
+	}
+
+	@Override
+	public Object clone() {
+		return null;
+	}
+
+	@Override
+	public boolean isEquivalent(Constraint c) {
+		return false;
+	}
+
+}
