ac2fb9a90af825456e42c2871fe9e0ac68e2ad40
Sebastian Stock <sestock@uos.de>
2014-09-07 14:42:33 +0200

    Removed unused array from Operator's constructor.
    The negative preconditions are set via negativeeffect property of the
    preconditions.

--- src/main/java/pfd0Symbolic/PFD0Operator.java
+++ src/main/java/pfd0Symbolic/PFD0Operator.java
@@ -37,9 +37,16 @@ public class PFD0Operator extends PlanReportroryItem {
 		this.logger = MetaCSPLogging.getLogger(PFD0Operator.class);
 	}
 	
+	@Deprecated
 	public PFD0Operator(String taskname, String[] arguments, PFD0Precondition[] preconditions, 
 			String[][] negativeEffects, VariablePrototype[] positiveEffects) {
 		this(taskname, arguments, preconditions, negativeEffects, positiveEffects, null, null);
+	}
+	
+
+	public PFD0Operator(String taskname, String[] arguments, PFD0Precondition[] preconditions, 
+			VariablePrototype[] positiveEffects) {
+		this(taskname, arguments, preconditions, null, positiveEffects, null, null);
 	}
 
 	
