b358b4c17704d45fa8de577db9ecb69a2930eb47
Sebastian Stock <sestock@uos.de>
2014-12-17 10:39:47 +0100

    Added JUnit test case for methods

---
+++ src/test/java/domains/JUnitTestRACEDomainMethods.java
@@ -0,0 +1,217 @@
+package domains;
+
+import static org.junit.Assert.assertTrue;
+import hybridDomainParsing.ProblemParser;
+import unify.CompoundSymbolicVariableConstraintSolver;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import pfd0Symbolic.FluentNetworkSolver;
+import pfd0Symbolic.PFD0Planner;
+
+public class JUnitTestRACEDomainMethods {
+	
+	private static String[][] symbols;
+	private static int[] ingredients;
+	private static final Map<String, String[]> typesInstancesMap = new HashMap<String, String[]>();
+	
+	private PFD0Planner planner;
+	private FluentNetworkSolver fluentSolver;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		symbols = RACEProblemsSingle.createSymbols();
+		ingredients = RACEProblemsSingle.createIngredients();
+		typesInstancesMap.put("ManipulationArea", new String[] {"manipulationAreaEastCounter1",
+				"manipulationAreaNorthTable1", "manipulationAreaSouthTable1",
+				"manipulationAreaWestTable2", "manipulationAreaEastTable2",});
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {}
+
+	@Before
+	public void setUp() throws Exception {
+		planner = new PFD0Planner(0,  600,  0, symbols, ingredients);
+		planner.setTypesInstancesMap(typesInstancesMap);
+
+		fluentSolver = (FluentNetworkSolver)planner.getConstraintSolvers()[0];
+		TestProblemParsing.initPlanner(planner, "domains/race_domain.ddl");
+	}
+
+	@After
+	public void tearDown() throws Exception {}
+
+	@Test
+	public void testMAdaptTorso() {
+		testProblem("problems/test_m_adapt_torso.pdl");
+	}
+	
+	@Test
+	public void testMTorsoAssumeDrivingPose0() {
+		testProblem("problems/test_m_torso_assume_driving_pose0.pdl");
+	}
+
+	@Test
+	public void testMTorsoAssumeDrivingPose1() {
+		testProblem("problems/test_m_torso_assume_driving_pose1.pdl");
+	}
+
+	@Test
+	public void testMAdaptArms0() {
+		testProblem("problems/test_m_adapt_arms_0.pdl");
+	}
+
+	@Test
+	public void testMAdaptArms1() {
+		testProblem("problems/test_m_adapt_arms_1.pdl");
+	}
+
+	@Test
+	public void testMAdaptArms2() {
+		testProblem("problems/test_m_adapt_arms_2.pdl");
+	}
+
+	@Test
+	public void testMArmsAssumeDrivingPose0() {
+		testProblem("problems/test_m_arms_assume_driving_pose0.pdl");
+	}
+
+	@Test
+	public void testMArmsAssumeDrivingPose1() {
+		testProblem("problems/test_m_arms_assume_driving_pose1.pdl");
+	}
+	
+	@Test
+	public void testMDriveRobot0() {
+		testProblem("problems/test_m_drive_robot_0.pdl");
+	}
+
+	@Test
+	public void testMDriveRobot1() {
+		testProblem("problems/test_m_drive_robot_1.pdl");
+	}
+
+	@Test
+	public void testMDriveRobot2() {
+		testProblem("problems/test_m_drive_robot_2.pdl");
+	}
+	
+	@Test
+	public void testMMoveBothArmsToSide1() {
+		testProblem("problems/test_m_move_both_arms_to_side_1.pdl");
+	}
+
+	@Test
+	public void testMMoveBothArmsToSide2() {
+		testProblem("problems/test_m_move_both_arms_to_side_2.pdl");
+	}
+
+	@Test
+	public void testMMoveBothArmsToSide3() {
+		testProblem("problems/test_m_move_both_arms_to_side_3.pdl");
+	}
+
+	@Test
+	public void testMMoveBothArmsToSide4() {
+		testProblem("problems/test_m_move_both_arms_to_side_4.pdl");
+	}
+
+	@Test
+	public void testMMoveBothArmsToSide5() {
+		testProblem("problems/test_m_move_both_arms_to_side_5.pdl");
+	}
+	
+	@Test
+	public void testMAssumeManipulationPose1() {
+		testProblem("problems/test_m_assume_manipulation_pose_1.pdl");
+	}
+
+	@Test
+	public void testMAssumeManipulationPose2() {
+		testProblem("problems/test_m_assume_manipulation_pose_2.pdl");
+	}
+
+	@Test
+	public void testMAssumeManipulationPose3() {
+		testProblem("problems/test_m_assume_manipulation_pose_3.pdl");
+	}
+	
+	@Test
+	public void testMLeaveManipulationPose1() {
+		testProblem("problems/test_m_leave_manipulation_pose_1.pdl");
+	}
+	
+	@Test
+	public void testMGraspObjectWArm1() {
+		testProblem("problems/test_m_grasp_object_w_arm_1.pdl");
+	}
+
+	@Test
+	public void testMGetObjectWArm2() {
+		testProblem("problems/test_m_get_object_w_arm_2.pdl");
+	}
+
+	@Test
+	public void testMGetObjectWArm3() {
+		testProblem("problems/test_m_get_object_w_arm_3.pdl");
+	}
+	
+	@Test
+	public void testMPutObject1() {
+		testProblem("problems/test_m_put_object_1.pdl");
+	}
+	
+	@Test
+	public void testMPutObject1a() {
+		testProblem("problems/test_m_put_object_1a.pdl");
+	}
+	
+	@Test
+	public void testMPutObject2() {
+		testProblem("problems/test_m_put_object_2.pdl");
+	}
+	
+	@Test
+	public void testMPutObject3() {
+		testProblem("problems/test_m_put_object_3.pdl");
+	}
+
+	@Test
+	public void testMMoveObject1() {
+		testProblem("problems/test_m_move_object_1.pdl");
+	}
+
+	@Test
+	public void testMMoveObject2() {
+		testProblem("problems/test_m_move_object_2.pdl");
+	}
+
+	@Test
+	public void testMMoveObject3() {
+		testProblem("problems/test_m_move_object_3.pdl");
+	}
+
+	@Test
+	public void testScenario_3_2_3() {
+		testProblem("problems/test_scenario_3_2_3.pdl");
+	}
+	
+	
+	private void testProblem(String problemPath) {
+		ProblemParser pp = new ProblemParser(problemPath);
+		pp.createState(fluentSolver);
+		((CompoundSymbolicVariableConstraintSolver) fluentSolver.getConstraintSolvers()[0]).propagateAllSub();
+		assertTrue(planner.backtrack());
+		TestProblemParsing.extractPlan(fluentSolver);
+	}
+
+
+}
