3358e1b1e404cdfa9e2bcc493b93d0594abd0724
Sebastian Stock <sestock@uos.de>
2014-12-23 16:03:43 +0100

    Adapted TestMoveBaseLookup

--- src/main/java/externalPahtPlanning/TestMoveBaseLookup.java
+++ src/main/java/externalPahtPlanning/TestMoveBaseLookup.java
@@ -1,96 +1,50 @@
 package externalPahtPlanning;
 
-import java.util.Vector;
+import hybridDomainParsing.ProblemParser;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 
-import org.metacsp.framework.ConstraintNetwork;
-import org.metacsp.framework.multi.MultiConstraintSolver;
 import org.metacsp.utility.logging.MetaCSPLogging;
 
-import domains.AAAIDomain;
-import domains.AAAIProblems;
 import pfd0Symbolic.FluentNetworkSolver;
 import pfd0Symbolic.PFD0Planner;
-import pfd0Symbolic.PlanReportroryItem;
-import pfd0Symbolic.TaskSelectionMetaConstraint;
 import unify.CompoundSymbolicVariableConstraintSolver;
+import domains.RACEProblemsSingle;
+import domains.TestProblemParsing;
 
 public class TestMoveBaseLookup {
 	
-	private static PFD0Planner planner;
-	private static FluentNetworkSolver fluentSolver;
 
 	public static void main(String[] args) {
-		String[][] symbols = AAAIDomain.createSymbols();
-		int[] ingredients = AAAIDomain.createIngredients();
 		
-		planner = new PFD0Planner(0,  600,  0, symbols, ingredients);
-		fluentSolver = (FluentNetworkSolver)planner.getConstraintSolvers()[0];
+		ProblemParser pp = new ProblemParser("problems/test_op_move_base.pdl");
 		
-		initMetaConstraints();
-		AAAIProblems.createProblemMoveBase(fluentSolver);
-//		AAAIProblems.createProblemMoveArmToSide(fluentSolver);
-//		AAAIProblems.createProblemMoveArmsToCarryposture(fluentSolver);
-//		AAAIProblems.createProblemTuckArms(fluentSolver);
-//		AAAIProblems.createProblemPickUpObject(fluentSolver);
-//		AAAIProblems.createProblemMoveTorso(fluentSolver);
-//		AAAIProblems.createProblemPlaceObject(fluentSolver);
+		String[][] symbols = RACEProblemsSingle.createSymbols();
+		int[] ingredients = RACEProblemsSingle.createIngredients();
 		
-//		AAAIProblems.createProblemDriveM(fluentSolver);
-//		AAAIProblems.createProblemGraspM(fluentSolver);
-//		AAAIProblems.createProblemGetObjectWithArmM(fluentSolver);
-		test();
-	}
-	
-	
-	private static void test() {
+		Map<String, String[]> typesInstancesMap = new HashMap<String, String[]>();
+		typesInstancesMap.put("ManipulationArea", new String[] {"manipulationAreaEastCounter1",
+				"manipulationAreaNorthTable1", "manipulationAreaSouthTable1",
+				"manipulationAreaWestTable2", "manipulationAreaEastTable2",});
+		
+		PFD0Planner planner = new PFD0Planner(0,  600,  0, symbols, ingredients);
+		planner.setTypesInstancesMap(typesInstancesMap);
+		FluentNetworkSolver fluentSolver = (FluentNetworkSolver)planner.getConstraintSolvers()[0];
+		
+		TestProblemParsing.initPlanner(planner, "domains/race_domain.ddl");
+		
+		pp.createState(fluentSolver);
+		
 		((CompoundSymbolicVariableConstraintSolver) fluentSolver.getConstraintSolvers()[0]).propagateAllSub();
-
 		
-		MetaCSPLogging.setLevel(Level.FINE);
+//		MetaCSPLogging.setLevel(Level.FINE);
+		MetaCSPLogging.setLevel(Level.OFF);
 		
-		long startTime = System.nanoTime();
-		System.out.println("Found a plan? " + planner.backtrack());
-		long endTime = System.nanoTime();
-
-		planner.draw();
-		ConstraintNetwork.draw(fluentSolver.getConstraintNetwork());
-		
-//		Callback cb = new Callback() {
-//			@Override
-//			public void performOperation() {
-//				long startTime = System.nanoTime();
-//				System.out.println("Found a plan? " + planner.backtrack());
-//				
-//				((CompoundSymbolicVariableConstraintSolver) fluentSolver.getConstraintSolvers()[0]).propagateAllSubFull();
-//				long endTime = System.nanoTime();
-//				System.out.println("Took "+((endTime - startTime) / 1000000) + " ms"); 
-//				planner.draw();
-//			}
-//		};
-//		ConstraintNetwork.draw(fluentSolver.getConstraintNetwork(), cb);
-		
-		
-		System.out.println(planner.getDescription());
-		ConstraintNetwork.draw(((MultiConstraintSolver)planner.getConstraintSolvers()[0]).getConstraintSolvers()[1].getConstraintNetwork());
-		
-		System.out.println("Took "+((endTime - startTime) / 1000000) + " ms"); 
-		
-		
-		System.out.println("Finished");
+		TestProblemParsing.plan(planner, fluentSolver);
+		TestProblemParsing.extractPlan(fluentSolver);
 	}
-	
-	private static void initMetaConstraints() {
-		TaskSelectionMetaConstraint selectionConstraint = new TaskSelectionMetaConstraint();
-		Vector<PlanReportroryItem> operators = AAAIDomain.createOperators(fluentSolver);
-		selectionConstraint.addOperators(operators);
-		Vector<PlanReportroryItem> methods = AAAIDomain.createMethods(fluentSolver);
-		selectionConstraint.addMethods(methods);
-		planner.addMetaConstraint(selectionConstraint);
-		
-		MoveBaseDurationEstimator mbEstimator = new LookUpTableDurationEstimator();
-		MoveBaseMetaConstraint mbConstraint = new MoveBaseMetaConstraint(mbEstimator);
-		planner.addMetaConstraint(mbConstraint);
-	}
+
 
 }
