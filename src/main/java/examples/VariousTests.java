50a3d2ccfdee03c9b7db7feecfb2540a0b3ca333
Sebastian Stock <sebastian.stock@gmail.com>
2016-07-03 11:56:55 +0200

    Added various tests for DWR

---
+++ src/main/java/examples/VariousTests.java
@@ -0,0 +1,237 @@
+package examples;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.logging.Level;
+
+import org.metacsp.framework.Constraint;
+import org.metacsp.framework.ConstraintNetwork;
+import org.metacsp.framework.ValueOrderingH;
+import org.metacsp.framework.Variable;
+import org.metacsp.utility.logging.MetaCSPLogging;
+
+import externalPathPlanning.LookUpTableDurationEstimator;
+import externalPathPlanning.MoveBaseDurationEstimator;
+import externalPathPlanning.MoveBaseMetaConstraint;
+import fluentSolver.Fluent;
+import fluentSolver.FluentConstraint;
+import fluentSolver.FluentNetworkSolver;
+import htn.HTNMetaConstraint;
+import htn.HTNPlanner;
+import htn.TaskApplicationMetaConstraint.markings;
+import htn.UnifyFewestsubsNewestbindingsValOH;
+import hybridDomainParsing.DomainParsingException;
+import hybridDomainParsing.HybridDomain;
+import hybridDomainParsing.PlanExtractor;
+import hybridDomainParsing.ProblemParser;
+import resourceFluent.FluentResourceUsageScheduler;
+import resourceFluent.FluentScheduler;
+import resourceFluent.ResourceUsageTemplate;
+import unify.CompoundSymbolicVariableConstraintSolver;
+
+public class VariousTests {
+	
+	// simplified version that combines w5 and w6 into one waypoint: tasks: bring(c12 p4) and bring(c22 p3)
+	// problematic that it tries r2 for both examples and runs into a deadend -> takes about 13 seconds
+//	static String ProblemPath = "domains/dwr/dwr_problem_straight-connected.pdl"; 
+
+	
+//	static String ProblemPath = "domains/dwr/dwr_test_examples/dwr_goto2.pdl";
+	
+	// An example that does not work with ordering relation between the two goal tasks:
+	static String ProblemPath = "domains/dwr/dwr_test_examples/dwr_switch_places.pdl";
+	
+	// This is an example that we either need to model the transition of state variables in the domain or use resources for robot activities.
+    // Otherwise !enter would start while !leave is not finished.
+//	static String ProblemPath = "domains/dwr/dwr_test_examples/dwr_leave-enter.pdl";
+
+	// simple example for testing the state variable scheduling
+//	static String ProblemPath = "domains/dwr/dwr_test_examples/test_state_scheduling_simple.pdl";
+
+	// one fluent should not be closed by two different tasks:
+//	static String ProblemPath = "domains/dwr/dwr_test_examples/test_same_negative_effects.pdl";
+
+	public static void main(String[] args) {
+//		Scanner s = new Scanner(System.in);
+//		System.out.println(s.nextInt());
+		
+		ProblemParser pp = new ProblemParser(ProblemPath);
+		
+		HybridDomain domain;
+		try {
+//			domain = new HybridDomain("domains/dwr/dwr_minduration.ddl");
+			domain = new HybridDomain("domains/dwr/dwr.ddl");
+//			domain = new HybridDomain("domains/dwr/dwr_state_scheduling.ddl");
+		} catch (DomainParsingException e) {
+			e.printStackTrace();
+			return;
+		}
+		int[] ingredients = new int[] {1, domain.getMaxArgs()};
+		String[][] symbols = new String[2][];
+		symbols[0] =  domain.getPredicateSymbols();
+		symbols[1] = pp.getArgumentSymbols();
+		Map<String, String[]> typesInstancesMap = pp.getTypesInstancesMap();
+		
+		HTNPlanner planner = new HTNPlanner(0,  600000,  0, symbols, ingredients);
+		planner.setTypesInstancesMap(typesInstancesMap);
+		
+		try {
+			initPlanner(planner, domain);
+		} catch (DomainParsingException e) {
+			System.out.println("Error while parsing domain: " + e.getMessage());
+			e.printStackTrace();
+			return;
+		}
+		
+		FluentNetworkSolver fluentSolver = (FluentNetworkSolver)planner.getConstraintSolvers()[0];
+		pp.createState(fluentSolver, domain);
+		((CompoundSymbolicVariableConstraintSolver) fluentSolver.getConstraintSolvers()[0]).propagateAllSub();
+		
+		
+//		MetaCSPLogging.setLevel(planner.getClass(), Level.FINEST);		
+//		MetaCSPLogging.setLevel(HTNMetaConstraint.class, Level.FINEST);
+
+//		MetaCSPLogging.setLevel(Level.FINE);
+		MetaCSPLogging.setLevel(Level.OFF);
+		
+//		planner.createInitialMeetsFutureConstraints();
+		plan(planner, fluentSolver);
+		
+		Variable[] allFluents = fluentSolver.getVariables();
+		ArrayList<Variable> plan = new ArrayList<Variable>();
+		int opCount = 0;
+		int mCount = 0;
+		for (Variable var : allFluents) {
+			String component = var.getComponent();
+			if (component == null) {
+				plan.add(var);
+			}
+			else if (component.equals("Activity")) {
+				plan.add(var);
+				opCount++;
+			} else if (component.equals("Task")) {
+				mCount++;
+			}
+			
+//			if (var.getMarking() == markings.OPEN) {
+//				System.out.println(var);
+//			}
+		}
+		System.out.println("#Ops: " + opCount);
+		System.out.println("#Compound Tasks: " + mCount);
+		System.out.println("#Fluents: " + fluentSolver.getVariables().length);
+		System.out.println("FluentConstraints: " + fluentSolver.getConstraints().length);
+
+		Variable[] planVector = plan.toArray(new Variable[plan.size()]);
+		Arrays.sort(planVector, new Comparator<Variable>() {
+			@Override
+			public int compare(Variable o1, Variable o2) {
+				// TODO Auto-generated method stub
+				Fluent f1 = (Fluent)o1;
+				Fluent f2 = (Fluent)o2;
+				return ((int)f1.getTemporalVariable().getEST()-(int)f2.getTemporalVariable().getEST());
+			}
+		});
+		
+		int c = 0;
+		for (Variable act : planVector) {
+			if (act.getComponent() != null)
+				System.out.println(c++ +".\t" + act);	
+		}
+		
+		extractPlan(fluentSolver);
+		
+		////////////////
+		
+	}
+	
+	public static boolean plan(HTNPlanner planner, FluentNetworkSolver fluentSolver) {
+		((CompoundSymbolicVariableConstraintSolver) fluentSolver.getConstraintSolvers()[0]).propagateAllSub();
+		
+		long startTime = System.nanoTime();
+		boolean result = planner.backtrack();
+		long endTime = System.nanoTime();
+		System.out.println("Found a plan? " + result);
+		
+
+		planner.draw();
+		
+		ConstraintNetwork cn = new ConstraintNetwork(null);
+		for (Constraint con : fluentSolver.getConstraintNetwork().getConstraints()) {
+			if (con instanceof FluentConstraint) {
+				FluentConstraint fc = (FluentConstraint) con;
+				if (fc.getType() == FluentConstraint.Type.MATCHES) {
+					fc.getFrom().setMarking(markings.UNIFIED);
+					cn.addConstraint(fc);
+				} else if (fc.getType() == FluentConstraint.Type.DC) {
+					cn.addConstraint(fc);
+				}
+			}
+		}
+		ConstraintNetwork.draw(cn);
+		
+		
+		ConstraintNetwork.draw(fluentSolver.getConstraintNetwork());
+		
+		ConstraintNetwork.draw(fluentSolver.getConstraintSolvers()[1].getConstraintNetwork());
+
+//		System.out.println(planner.getDescription());
+		System.out.println("Took "+((endTime - startTime) / 1000000) + " ms"); 
+		System.out.println("Finished");
+		
+//		System.out.println("AGAIN");
+//		startTime = System.nanoTime();
+//		result = planner.backtrack();
+//		System.out.println("Found a plan? " + result);
+//		endTime = System.nanoTime();
+//
+//		System.out.println("Took "+((endTime - startTime) / 1000000) + " ms"); 
+//		System.out.println("Finished");
+		
+		return result;
+	}
+	
+	public static void extractPlan(FluentNetworkSolver fluentSolver) {
+		PlanExtractor planEx = new PlanExtractor(fluentSolver);
+		planEx.printPlan();
+	}
+	
+	
+	public static void initPlanner(HTNPlanner planner, HybridDomain domain) throws DomainParsingException {
+		// load domain
+		domain.parseDomain(planner);
+		
+		// init meta constraints based on domain
+//		ValueOrderingH valOH = new NewestFluentsValOH();
+		ValueOrderingH valOH = new UnifyFewestsubsNewestbindingsValOH();
+		
+		HTNMetaConstraint htnConstraint = new HTNMetaConstraint(valOH);
+		htnConstraint.addOperators(domain.getOperators());
+		htnConstraint.addMethods(domain.getMethods());
+		Vector<ResourceUsageTemplate> fluentResourceUsages = domain.getFluentResourceUsages();
+		htnConstraint.setResourceUsages(fluentResourceUsages);
+		
+		
+		for (FluentScheduler fs : domain.getFluentSchedulers()) {
+			planner.addMetaConstraint(fs);
+		}
+		
+		for (FluentResourceUsageScheduler rs : domain.getResourceSchedulers()) {
+			planner.addMetaConstraint(rs);
+		}
+		
+		MoveBaseDurationEstimator mbEstimator = new LookUpTableDurationEstimator();
+		MoveBaseMetaConstraint mbConstraint = new MoveBaseMetaConstraint(mbEstimator);
+		planner.addMetaConstraint(mbConstraint);
+		
+		planner.addMetaConstraint(htnConstraint);
+	}
+	
+
+
+
+}
