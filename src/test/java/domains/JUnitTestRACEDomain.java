2205285c7902946f42ed373d0fc51afe3a6c52b3
Sebastian Stock <sebastian.stock@gmail.com>
2020-05-08 10:39:05 +0200

    Catch exceptions in junit tests

--- src/test/java/domains/JUnitTestRACEDomain.java
+++ src/test/java/domains/JUnitTestRACEDomain.java
@@ -1,5 +1,6 @@
 package domains;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -11,71 +12,69 @@ import hybridDomainParsing.DomainParsingException;
 import planner.CHIMP;
 
 public class JUnitTestRACEDomain {
-	
-	private String domainFile = "domains/ordered_domain.ddl";
-	
-	public void testPlanning(String problemFile) {
-		ValueOrderingH valOH = new UnifyDeepestWeightNewestbindingsValOH();
-		
-		CHIMP.CHIMPBuilder builder = new CHIMP.CHIMPBuilder(domainFile, problemFile)
-				.valHeuristic(valOH)
-				.mbEstimator(new LookUpTableDurationEstimator())
-				.htnUnification(true);
-		builder.htnUnification(true);
-		CHIMP chimp;
-		try {
-			chimp = builder.build();
-		} catch (DomainParsingException e) {
-			e.printStackTrace();
-			return;
-		}
-		
-		assertTrue(chimp.generatePlan());
-	}
 
-	@Test
-	public void testOpTuckArms() {
-		testPlanning("problems/test_op_tuck_arms.pdl");
-	}
-	
-	@Test
-	public void testOpMoveBase() {
-		testPlanning("problems/test_op_move_base.pdl");
-	}
-	
-	@Test
-	public void testOpMoveTorso() {
-		testPlanning("problems/test_op_move_torso.pdl");
-	}
+    private String domainFile = "domains/ordered_domain.ddl";
 
-	@Test
-	public void testOpMoveBaseBlind() {
-		testPlanning("problems/test_op_move_base_blind.pdl");
-	}
-	
-	@Test
-	public void testOpPickUpObject() {
-		testPlanning("problems/test_op_pick_up_object.pdl");
-	}
-	
-	@Test
-	public void testOpPlaceObject() {
-		testPlanning("problems/test_op_place_object.pdl");
-	}
-		
-	@Test
-	public void testOpMoveArmToSide() {
-		testPlanning("problems/test_op_move_arm_to_side.pdl");
-	}
-			
-	@Test
-	public void testMoveArmsToCarryposture() {
-		testPlanning("problems/test_op_move_arms_to_carryposture.pdl");
-	}
-				
-	@Test
-	public void testOpObserveObjectsOnArea() {
-		testPlanning("problems/test_op_observe_objects_on_area.pdl");
-	}
+    public void testPlanning(String problemFile) {
+        ValueOrderingH valOH = new UnifyDeepestWeightNewestbindingsValOH();
+        try {
+            CHIMP.CHIMPBuilder builder = new CHIMP.CHIMPBuilder(domainFile, problemFile)
+                    .valHeuristic(valOH)
+                    .mbEstimator(new LookUpTableDurationEstimator())
+                    .htnUnification(true);
+            builder.htnUnification(true);
+            CHIMP chimp = builder.build();
+            assertTrue(chimp.generatePlan());
+        } catch (DomainParsingException e) {
+            e.printStackTrace();
+            assertFalse(true);
+            return;
+        }
+    }
+
+    @Test
+    public void testOpTuckArms() {
+        testPlanning("problems/test_op_tuck_arms.pdl");
+    }
+
+    @Test
+    public void testOpMoveBase() {
+        testPlanning("problems/test_op_move_base.pdl");
+    }
+
+    @Test
+    public void testOpMoveTorso() {
+        testPlanning("problems/test_op_move_torso.pdl");
+    }
+
+    @Test
+    public void testOpMoveBaseBlind() {
+        testPlanning("problems/test_op_move_base_blind.pdl");
+    }
+
+    @Test
+    public void testOpPickUpObject() {
+        testPlanning("problems/test_op_pick_up_object.pdl");
+    }
+
+    @Test
+    public void testOpPlaceObject() {
+        testPlanning("problems/test_op_place_object.pdl");
+    }
+
+    @Test
+    public void testOpMoveArmToSide() {
+        testPlanning("problems/test_op_move_arm_to_side.pdl");
+    }
+
+    @Test
+    public void testMoveArmsToCarryposture() {
+        testPlanning("problems/test_op_move_arms_to_carryposture.pdl");
+    }
+
+    @Test
+    public void testOpObserveObjectsOnArea() {
+        testPlanning("problems/test_op_observe_objects_on_area.pdl");
+    }
 
 }
--- src/test/java/transterra_domains/JUnitTestTransterraDomain.java
+++ src/test/java/transterra_domains/JUnitTestTransterraDomain.java
@@ -11,96 +11,93 @@ import hybridDomainParsing.DomainParsingException;
 import planner.CHIMP;
 
 public class JUnitTestTransterraDomain {
-	
-	private String domainFile = "domains/transterra_v1.ddl";
-	
-	private void testPlanning(String problemFile) {
-		ValueOrderingH valOH = new UnifyDeepestWeightNewestbindingsValOH();
-		
-		CHIMP.CHIMPBuilder builder = new CHIMP.CHIMPBuilder(domainFile, problemFile)
-				.valHeuristic(valOH)
-				.mbEstimator(new LookUpTableDurationEstimator())
-				.htnUnification(true);
-		builder.htnUnification(true);
-		CHIMP chimp;
-		try {
-			chimp = builder.build();
-		} catch (DomainParsingException e) {
-			e.printStackTrace();
-			return;
-		}
-		
-		assertTrue(chimp.generatePlan());
-	}
-	
 
-	@Test
-	public void testOpMoveTo() {
-		testPlanning("problems/transterra_problems_v1/test_op_move_to.pdl");
-	}
+    private String domainFile = "domains/transterra_v1.ddl";
 
-	@Test
-	public void testOpSample() {
-		testPlanning("problems/transterra_problems_v1/test_op_sample_regolith.pdl");
-	}
+    private void testPlanning(String problemFile) {
+        ValueOrderingH valOH = new UnifyDeepestWeightNewestbindingsValOH();
+        try {
+            CHIMP.CHIMPBuilder builder = new CHIMP.CHIMPBuilder(domainFile, problemFile)
+                    .valHeuristic(valOH)
+                    .mbEstimator(new LookUpTableDurationEstimator())
+                    .htnUnification(true);
+            builder.htnUnification(true);
+            CHIMP chimp = builder.build();
+            assertTrue(chimp.generatePlan());
+        } catch (DomainParsingException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
 
-	@Test
-	public void testOpTransferSample() {
-		testPlanning("problems/transterra_problems_v1/test_op_transfer_sample.pdl");
-	}
 
-	@Test
-	public void testOpTransferPayload() {
-		testPlanning("problems/transterra_problems_v1/test_op_transfer_payload.pdl");
-	}
+    @Test
+    public void testOpMoveTo() {
+        testPlanning("problems/transterra_problems_v1/test_op_move_to.pdl");
+    }
 
-	@Test
-	public void testOpPickupBasecamp() {
-		testPlanning("problems/transterra_problems_v1/test_op_pickup_basecamp.pdl");
-	}
+    @Test
+    public void testOpSample() {
+        testPlanning("problems/transterra_problems_v1/test_op_sample_regolith.pdl");
+    }
 
-	@Test
-	public void testOpPickupBasecamp2() {
-		testPlanning("problems/transterra_problems_v1/test_op_pickup_basecamp_2.pdl");
-	}
+    @Test
+    public void testOpTransferSample() {
+        testPlanning("problems/transterra_problems_v1/test_op_transfer_sample.pdl");
+    }
 
-	@Test
-	public void testOpPlaceBasecamp() {
-		testPlanning("problems/transterra_problems_v1/test_op_place_basecamp.pdl");
-	}
+    @Test
+    public void testOpTransferPayload() {
+        testPlanning("problems/transterra_problems_v1/test_op_transfer_payload.pdl");
+    }
 
-	@Test
-	public void testMDeployBasecamp1() {
-		testPlanning("problems/transterra_problems_v1/test_m_deploy_basecamp_1.pdl");
-	}
+    @Test
+    public void testOpPickupBasecamp() {
+        testPlanning("problems/transterra_problems_v1/test_op_pickup_basecamp.pdl");
+    }
 
-	@Test
-	public void testMDeployBasecamp2() {
-		testPlanning("problems/transterra_problems_v1/test_m_deploy_basecamp_2.pdl");
-	}
+    @Test
+    public void testOpPickupBasecamp2() {
+        testPlanning("problems/transterra_problems_v1/test_op_pickup_basecamp_2.pdl");
+    }
 
-	@Test
-	public void testMTakeSamples1() {
-		testPlanning("problems/transterra_problems_v1/test_m_take_samples_1.pdl");
-	}
+    @Test
+    public void testOpPlaceBasecamp() {
+        testPlanning("problems/transterra_problems_v1/test_op_place_basecamp.pdl");
+    }
 
-	@Test
-	public void testMTakeSamples2() {
-		testPlanning("problems/transterra_problems_v1/test_m_take_samples_2.pdl");
-	}
+    @Test
+    public void testMDeployBasecamp1() {
+        testPlanning("problems/transterra_problems_v1/test_m_deploy_basecamp_1.pdl");
+    }
 
-	@Test
-	public void testMGetBasecamp1() {
-		testPlanning("problems/transterra_problems_v1/test_m_get_basecamp_1.pdl");
-	}
+    @Test
+    public void testMDeployBasecamp2() {
+        testPlanning("problems/transterra_problems_v1/test_m_deploy_basecamp_2.pdl");
+    }
 
-	@Test
-	public void testMGetBasecamp2() {
-		testPlanning("problems/transterra_problems_v1/test_m_get_basecamp_2.pdl");
-	}
-	
-	@Test
-	public void testMTransferFilled() {
-		testPlanning("problems/transterra_problems_v1/test_m_transfer_filled.pdl");
-	}
+    @Test
+    public void testMTakeSamples1() {
+        testPlanning("problems/transterra_problems_v1/test_m_take_samples_1.pdl");
+    }
+
+    @Test
+    public void testMTakeSamples2() {
+        testPlanning("problems/transterra_problems_v1/test_m_take_samples_2.pdl");
+    }
+
+    @Test
+    public void testMGetBasecamp1() {
+        testPlanning("problems/transterra_problems_v1/test_m_get_basecamp_1.pdl");
+    }
+
+    @Test
+    public void testMGetBasecamp2() {
+        testPlanning("problems/transterra_problems_v1/test_m_get_basecamp_2.pdl");
+    }
+
+    @Test
+    public void testMTransferFilled() {
+        testPlanning("problems/transterra_problems_v1/test_m_transfer_filled.pdl");
+    }
 }
