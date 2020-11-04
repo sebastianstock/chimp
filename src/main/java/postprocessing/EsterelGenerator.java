001b6b2c49c6bae66e4c16de7c3060473ba2866a
Sebastian Stock <sebastian.stock@gmail.com>
2020-11-04 16:42:54 +0100

    Clean-up esterel generator

--- src/main/java/postprocessing/EsterelGenerator.java
+++ src/main/java/postprocessing/EsterelGenerator.java
@@ -3,6 +3,7 @@ package postprocessing;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 import fluentSolver.Fluent;
+import fluentSolver.FluentNetworkSolver;
 import org.metacsp.framework.Constraint;
 import org.metacsp.framework.ConstraintNetwork;
 import org.metacsp.framework.Variable;
@@ -16,117 +17,31 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.io.Writer;
-import java.sql.Time;
 import java.util.*;
 
 public class EsterelGenerator {
 
     public static void generateEsterelGraph(CHIMP chimp, Writer writer) {
-        AllenIntervalNetworkSolver aiSolver =
-                (AllenIntervalNetworkSolver) chimp.getFluentSolver().getConstraintSolvers()[1];
-        APSPSolver apspSolver = (APSPSolver) aiSolver.getConstraintSolvers()[0];
-
-        // Copy temporal constraint network to dummy cn
-        ConstraintNetwork cn = (ConstraintNetwork) apspSolver.getConstraintNetwork().clone();
-
-        // [0,0]-constraints are relevant in both directions, therefore we also need edges in both directions
-        // Add reverse constraints for constraints [0,0]-constraints from end-timepoint to end-timepoint
-        List<SimpleDistanceConstraint> additionalConsList = new ArrayList<>();
-        for (Constraint con : cn.getConstraints()) {
-            SimpleDistanceConstraint dst = (SimpleDistanceConstraint) con;
-            if (isEndpoint(dst.getFrom()) && isEndpoint(dst.getTo()) &&
-                    dst.getMinimum() == 0 && dst.getMaximum() == 0) {
-                additionalConsList.add(createReverseConstraint(dst));
-            }
-        }
-        cn.addConstraints(additionalConsList.toArray(new SimpleDistanceConstraint[additionalConsList.size()]));
-
-        // Replace fluents that are not activities
-        for (Variable v : chimp.getFluentSolver().getVariables()) {
-            Fluent fl = (Fluent) v;
-            if (!fl.isActivity()) {
-                TimePoint start = fl.getAllenInterval().getStart();
-                additionalConsList.addAll(replaceNode(cn, start));
-                TimePoint end = fl.getAllenInterval().getEnd();
-                additionalConsList.addAll(replaceNode(cn, end));
-            }
-        }
+        FluentNetworkSolver fluentSolver = chimp.getFluentSolver();
+        List<SimpleDistanceConstraint> additionalConsList = calcAdditionalConstraints(fluentSolver);
 
         // Add new constraints to apspSolver
-        System.out.println("Adding " + additionalConsList.size() + " constraints");
+        APSPSolver apspSolver = getApspSolver(fluentSolver);
         boolean success = apspSolver.addConstraints(
                 additionalConsList.toArray(new SimpleDistanceConstraint[additionalConsList.size()]));
-        System.out.println("Success? " + success);
-
-        // search for constraints between activities
-        TimePoint tp12 = (TimePoint) apspSolver.getVariable(12);
-        TimePoint tp13 = (TimePoint) apspSolver.getVariable(13);
-        TimePoint tp14 = (TimePoint) apspSolver.getVariable(14);
-        TimePoint tp15 = (TimePoint) apspSolver.getVariable(15);
-        SimpleDistanceConstraint dst14To15 = apspSolver.getConstraint(tp14, tp15);
-        System.out.println(dst14To15.toString());
-
-        System.out.println("Searching for 12-13: " + apspSolver.getConstraint(tp12, tp13).toString());
-        System.out.println("Searching for 14-12: " + apspSolver.getConstraint(tp14, tp12).toString());
-        System.out.println("Searching for 15-12: " + apspSolver.getConstraint(tp15, tp12).toString());
-//            System.out.println(apspSolver.getConstraint(tp12, tp14).toString());
-//            System.out.println("Searching for 13-12: " + apspSolver.getConstraint(tp13, tp12).toString());
-
-        for (Constraint con : apspSolver.getConstraintNetwork().getConstraints()) {
-            SimpleDistanceConstraint dstCon = (SimpleDistanceConstraint) con;
-            int fromID = dstCon.getFrom().getID();
-            int toID = dstCon.getTo().getID();
-            if (fromID >= 12 && fromID <=15 && toID >=12 && toID <=15) { // test if both are activities
-                System.out.println(dstCon.toString());
-            }
-        }
 
         // Create Esterel Graph:
 
-        // Create map/array TimepointId -> FluentId
-        System.out.println("Creating timePointActivityMap");
-        Map<TimePoint, Fluent> timePointActivityMap = new HashMap<>();
-        for (Variable activityVar : chimp.getFluentSolver().getVariables(Fluent.ACTIVITY_TYPE_STR))  {
-            Fluent activity = (Fluent) activityVar;
-            timePointActivityMap.put(activity.getAllenInterval().getStart(), activity);
-            timePointActivityMap.put(activity.getAllenInterval().getEnd(), activity);
-            System.out.println("Added " + activity.toString());
-        }
+        Map<TimePoint, Fluent> timePointActivityMap = getTimePointActivityMap(fluentSolver);
 
         // create ActionDispatch messages
-        Map<Fluent, ActionDispatch> fluentActionDispatchMap = new HashMap<>();
-        for (Fluent fl : timePointActivityMap.values()) {
-            ActionDispatch actionDispatch = new ActionDispatch(fl.getID(),
-                    fl.getCompoundSymbolicVariable().getPredicateName());
-            actionDispatch.duration = (fl.getAllenInterval().getEET() - fl.getAllenInterval().getEST()) / 1000;
-            String[] flArgs = fl.getCompoundSymbolicVariable().getArgs();
-            for (int i = 0; i < flArgs.length; i++) {
-                String key = "v" + i;
-                actionDispatch.parameters.add(new KeyValue(key, flArgs[i]));
-            }
-            actionDispatch.dispatch_time = fl.getAllenInterval().getEST() / 1000;
-            fluentActionDispatchMap.put(fl, actionDispatch);
-        }
-
-        // Write nodes
+        Map<Fluent, ActionDispatch> fluentActionDispatchMap = getFluentActionDispatchMap(timePointActivityMap);
 
         // Create and sort array of timepoints
-        TimePoint[] tps = new TimePoint[timePointActivityMap.keySet().size() + 2];
-        tps[0] = apspSolver.getSource();
-        tps[tps.length-1] = apspSolver.getSink();
-        int pos = 1;
-        for (TimePoint tp : timePointActivityMap.keySet()) {
-            tps[pos++] = tp;
-        }
-        Arrays.sort(tps, new Comparator<TimePoint>() {
-            @Override
-            public int compare(TimePoint t0, TimePoint t1) {
-                return Long.compare(t0.getLowerBound(), t1.getLowerBound());
-            }
-        });
+        TimePoint[] tps = createSortedActivityTimepoints(apspSolver, timePointActivityMap);
 
-        // add nodes to esterel paln
         EsterelPlan esterelPlan = new EsterelPlan();
+        // add nodes to esterel plan
         for (int i = 0; i < tps.length - 1; i++) {
             TimePoint tp = tps[i];
             if (tp.getID() == 0) {
@@ -152,15 +67,12 @@ public class EsterelGenerator {
                 esterelPlan.nodes.add(node);
             }
         }
-
         // the index of nodes in the esterel plan is the same as that of the corresponding timepoint in tps
 
-        // Write edges
-        // for two nodes check if an edge exist and write
-
+        // Add edges: for two nodes check if an edge exist and add it to the plan
         int edgeIdCnt = 0;
         for (int i = 0; i < tps.length - 1; i++) {
-            for (int j = 0; j < tps.length - 1; j++) {
+            for (int j = 0; j < tps.length - 1; j++) {  // horizon tp is last in tps and can be ignored
                 SimpleDistanceConstraint sdc = apspSolver.getConstraint(tps[i], tps[j]);
                 if (sdc != null) {
                     System.out.println("Found constraint: " + i + " -> " + j + " : " + sdc);
@@ -169,11 +81,9 @@ public class EsterelGenerator {
                     EsterelPlanEdge edge = new EsterelPlanEdge(edgeId, "edge" + edgeId, i, j,
                             sdc.getMinimum() / 1000, sdc.getMaximum() / 1000);
                     esterelPlan.addEdge(edge);
-
                 }
             }
         }
-
 
         // export esterel
         ObjectMapper objectMapper = new ObjectMapper(
@@ -183,6 +93,92 @@ public class EsterelGenerator {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static TimePoint[] createSortedActivityTimepoints(APSPSolver apspSolver, Map<TimePoint, Fluent> timePointActivityMap) {
+        TimePoint[] tps = new TimePoint[timePointActivityMap.keySet().size() + 2];
+        tps[0] = apspSolver.getSource();
+        tps[tps.length-1] = apspSolver.getSink();
+        int pos = 1;
+        for (TimePoint tp : timePointActivityMap.keySet()) {
+            tps[pos++] = tp;
+        }
+        Arrays.sort(tps, new Comparator<TimePoint>() {
+            @Override
+            public int compare(TimePoint t0, TimePoint t1) {
+                return Long.compare(t0.getLowerBound(), t1.getLowerBound());
+            }
+        });
+        return tps;
+    }
+
+    public static Map<Fluent, ActionDispatch> getFluentActionDispatchMap(Map<TimePoint, Fluent> timePointActivityMap) {
+        Map<Fluent, ActionDispatch> fluentActionDispatchMap = new HashMap<>();
+        for (Fluent fl : timePointActivityMap.values()) {
+            ActionDispatch actionDispatch = new ActionDispatch(fl.getID(),
+                    fl.getCompoundSymbolicVariable().getPredicateName());
+            actionDispatch.duration = (fl.getAllenInterval().getEET() - fl.getAllenInterval().getEST()) / 1000;
+            String[] flArgs = fl.getCompoundSymbolicVariable().getArgs();
+            for (int i = 0; i < flArgs.length; i++) {
+                String key = "v" + i;
+                actionDispatch.parameters.add(new KeyValue(key, flArgs[i]));
+            }
+            actionDispatch.dispatch_time = fl.getAllenInterval().getEST() / 1000;
+            fluentActionDispatchMap.put(fl, actionDispatch);
+        }
+        return fluentActionDispatchMap;
+    }
+
+    public static Map<TimePoint, Fluent> getTimePointActivityMap(FluentNetworkSolver fluentSolver) {
+        Map<TimePoint, Fluent> timePointActivityMap = new HashMap<>();
+        for (Variable activityVar : fluentSolver.getVariables(Fluent.ACTIVITY_TYPE_STR))  {
+            Fluent activity = (Fluent) activityVar;
+            timePointActivityMap.put(activity.getAllenInterval().getStart(), activity);
+            timePointActivityMap.put(activity.getAllenInterval().getEnd(), activity);
+        }
+        return timePointActivityMap;
+    }
+
+    public static APSPSolver getApspSolver(FluentNetworkSolver fluentSolver) {
+        AllenIntervalNetworkSolver aiSolver =
+                (AllenIntervalNetworkSolver) fluentSolver.getConstraintSolvers()[1];
+        APSPSolver apspSolver = (APSPSolver) aiSolver.getConstraintSolvers()[0];
+        return apspSolver;
+    }
+
+    /**
+     * Compute implicit constraints in between activities for between which a path exists.
+     * @param fluentSolver
+     * @return List of implicit constraints between activities.
+     */
+    public static List<SimpleDistanceConstraint> calcAdditionalConstraints(FluentNetworkSolver fluentSolver) {
+        APSPSolver apspSolver = getApspSolver(fluentSolver);
+        // Copy temporal constraint network to dummy cn
+        ConstraintNetwork cn = (ConstraintNetwork) apspSolver.getConstraintNetwork().clone();
+
+        List<SimpleDistanceConstraint> additionalConsList = new ArrayList<>();
+        // [0,0]-constraints are relevant in both directions, therefore we also need edges in both directions
+        // Add reverse constraints for constraints [0,0]-constraints from end-timepoint to end-timepoint
+        for (Constraint con : cn.getConstraints()) {
+            SimpleDistanceConstraint dst = (SimpleDistanceConstraint) con;
+            if (isEndpoint(dst.getFrom()) && isEndpoint(dst.getTo()) &&
+                    dst.getMinimum() == 0 && dst.getMaximum() == 0) {
+                additionalConsList.add(createReverseConstraint(dst));
+            }
+        }
+        cn.addConstraints(additionalConsList.toArray(new SimpleDistanceConstraint[additionalConsList.size()]));
+
+        // Replace fluents that are not activities
+        for (Variable v : fluentSolver.getVariables()) {
+            Fluent fl = (Fluent) v;
+            if (!fl.isActivity()) {
+                TimePoint start = fl.getAllenInterval().getStart();
+                additionalConsList.addAll(replaceNode(cn, start));
+                TimePoint end = fl.getAllenInterval().getEnd();
+                additionalConsList.addAll(replaceNode(cn, end));
+            }
+        }
+        return additionalConsList;
     }
 
     //
